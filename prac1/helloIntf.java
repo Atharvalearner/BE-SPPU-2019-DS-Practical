7c67223f600ccf5cf46886edeebb4e345f730a34
Atharva Banchhode <98328628+Atharvalearner@users.noreply.github.com>
2025-05-08 08:29:36 +0530

    Delete prac1 directory

--- prac1/helloIntf.java
+++
@@ -1,8 +0,0 @@
-package prac1;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface helloIntf extends Remote{      // Remote interface which declares the remote method
-    String sayHello(String msg) throws RemoteException;
-}
